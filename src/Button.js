bf81c9476d707b3962e8800e51d3f4b401f590c7
Zeqing Zhang <zzq889@gmail.com>
2016-05-10 22:37:07 +0800

    update button style

--- src/Button.js
+++ src/Button.js
@@ -5,20 +5,20 @@ import color from 'color';
 const styles = {
   base: {
     color: '#fff',
+  },
 
-    // Adding interactive state couldn't be easier! Add a special key to your
-    // style object (:hover, :focus, :active, or @media) with the additional rules.
+  primary: {
+    background: '#0074D9',
     ':hover': {
       background: color('#0074d9').lighten(0.2).hexString()
     }
   },
 
-  primary: {
-    background: '#0074D9'
-  },
-
   warning: {
-    background: '#FF4136'
+    background: '#FF4136',
+    ':hover': {
+      background: color('#FF4136').lighten(0.2).hexString()
+    }
   }
 };
 
