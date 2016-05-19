5df99c249d9d5b5c0e764a3bfc2aa61cca28d00b
Yann Peng <yann.peng@legensity.com>
2016-05-19 12:20:26 +0800

    darktheme name update

--- src/stories/Button.js
+++ src/stories/Button.js
@@ -5,7 +5,7 @@ import themes from '../styles/themes';
 import darkBaseTheme from '../styles/baseThemes/darkBaseTheme';
 import Button from '../Button/Button';
 
-const theme = themes.getTheme(darkBaseTheme);
+const darkTheme = themes.getTheme(darkBaseTheme);
 
 storiesOf('Button', module)
   .add('FlatButton', () => (
@@ -20,7 +20,7 @@ storiesOf('Button', module)
           backgroundColor="#607d8b" hoverColor="#bcaaa4"
         >Customized Color</Button>
       </div>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={darkTheme}>
         <div style={{ backgroundColor: '#263238' }}>
           <Button onClick={ action('button clicked') }>Default</Button>
           <Button onClick={ action('button clicked') } kind="primary">Primary</Button>
@@ -52,7 +52,7 @@ storiesOf('Button', module)
           design="raisedButton" backgroundColor="#607d8b" hoverColor="#bcaaa4"
         >Customized Color</Button>
       </div>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={darkTheme}>
         <div style={{ backgroundColor: '#263238' }}>
           <Button onClick={ action('button clicked') } design="raisedButton">Default</Button>
           <Button onClick={ action('button clicked') }
